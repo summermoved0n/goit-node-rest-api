4edd0b63e52ca26677a2de00a415c283c56815ab
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-03-03 19:20:37 -0500

    added changes after hw check

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -20,7 +20,6 @@ const getOneContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const { id } = req.params;
-  console.log(req.body);
   const result = await contactsService.removeContact(id);
   if (!result) {
     throw HttpError(404);
@@ -31,19 +30,13 @@ const deleteContact = async (req, res) => {
 
 const createContact = async (req, res) => {
   const result = await contactsService.addContact(req.body);
-  console.log(req.body);
-  console.log(result);
 
   res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
-  if (Object.keys(req.body).length === 0) {
-    throw HttpError(400, "Body must have at least one field");
-  }
   const { id } = req.params;
   const result = await contactsService.updateContactById(id, req.body);
-  console.log(req.body);
 
   if (!result) {
     throw HttpError(404);
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -10,4 +10,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-});
+})
+  .min(1)
+  .messages({ "object.min": "Body must have at least one field" });
