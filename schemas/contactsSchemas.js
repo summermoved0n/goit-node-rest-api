1468c8b89e2aa7a0159a436e4f4b809d1ed15955
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-04-10 16:10:40 -0400

    add

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,6 +4,7 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.string(),
 });
 
 export const updateContactSchema = Joi.object({
